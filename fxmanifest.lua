3174769f7c555b6b7aef8721491d8a78b5d30376
CFOUR <116594943+c-four77@users.noreply.github.com>
2023-12-17 02:04:10 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,27 +1,23 @@
-name "mustache-aitaxi"
-author "mustache_dom"
-description "AI Taxi by mustache dom"
-fx_version "cerulean"
-game "gta5"
+fx_version 'cerulean'
+game 'gta5'
+
+name 'md-aitaxi'
+author 'Mustache_dom'
+
+description 'AI Taxi by mustache dom'
 
 client_scripts {
 	'client/**.lua',
-	
 }
 
 server_scripts {
     'server/**.lua',
-	
 }
 
 shared_scripts {
     'config.lua',
-	 '@ox_lib/init.lua',
-	
-	
+	'@ox_lib/init.lua'
 }
 
-
 lua54 'yes'
-
 dependency '/assetpacks'
